214df8cb6b85be2f6b3c8f3b61c109a50d83b3ef
Gilberto Gabon <gagabon501@yahoo.com>
2024-01-12 11:11:47 +1300

    Better scrolling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
+import Card from "./components/Card";
+
 const logo = "GAG_Profile.jpg";
+
 function App() {
   return (
     <>
-      <div className="overscroll-none relative">
-        <nav className="flex justify-between  text-white w-full fixed top-0 p-2  bg-black z-50">
-          <a href="#" className="">
+      <div className="gcontainer">
+        <nav className="flex justify-between flex-wrap text-white w-full sticky mx-auto top-0 p-2  bg-black">
+          <a href="#about" className="">
             <img
               src={logo}
               className="w-100 h-10 mr-2 rounded-full"
               alt="Logo"
             />
           </a>
-          <div className="grid grid-cols-3 gap-4 align-middle">
-            <a href="#about" className="p-2">
+          <div className="flex justify-between align-middle">
+            <a
+              href="#about"
+              className="p-2 hover:decoration-solid hover:underline"
+            >
               about me
             </a>
 
-            <a href="#mywork" className="p-2">
+            <a
+              href="#mywork"
+              className="p-2 hover:decoration-solid hover:underline"
+            >
               my work
             </a>
 
-            <a href="#contact" className="p-2">
+            <a
+              href="#contact"
+              className="p-2 hover:decoration-solid hover:underline"
+            >
               contact me
             </a>
           </div>
@@ -28,21 +40,23 @@ function App() {
 
         <section
           id="about"
-          className="h-screen  bg-blue-100 sticky top-14 flex flex-col justify-center items-center"
+          className="bg-blue-100 flex flex-col align-middle justify-center"
         >
           <div className="text-6xl text-center py-4">
             Hi! I'm Gilberto Gabon
           </div>
           <div className="text-4xl text-center">Fullstack Developer</div>
         </section>
-        <section id="mywork" className="h-screen bg-blue-300  sticky top-14">
+        <section id="mywork" className="bg-blue-300  ">
           my work
         </section>
-        <section id="contact" className="h-screen  bg-blue-400  sticky top-14">
+        <section id="contact" className="bg-blue-400  ">
           contact me
         </section>
 
-        <footer className="sticky bottom-5">Copyright</footer>
+        <footer className="sticky bottom-0 p-2 bg-black text-white text-center w-full">
+          Copyright
+        </footer>
       </div>
     </>
   );

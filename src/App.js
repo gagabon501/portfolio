742f1240d24ee445b4f34c5b096a2c3ffc548630
Gilberto Gabon <gagabon501@yahoo.com>
2024-01-14 20:08:21 +1300

    Used NavBar component for the navigation

--- src/App.js
+++ src/App.js
@@ -1,63 +1,80 @@
 import Card from "./components/Card";
-
-const logo = "GAG_Profile.jpg";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
-      <div className="gcontainer">
-        <nav className="flex justify-between flex-wrap text-white w-full sticky mx-auto top-0 p-2  bg-black">
-          <a href="#about" className="">
-            <img
-              src={logo}
-              className="w-100 h-10 mr-2 rounded-full"
-              alt="Logo"
-            />
-          </a>
-          <div className="flex justify-between align-middle">
-            <a
-              href="#about"
-              className="p-2 hover:decoration-solid hover:underline"
-            >
-              about me
-            </a>
-
-            <a
-              href="#mywork"
-              className="p-2 hover:decoration-solid hover:underline"
-            >
-              my work
-            </a>
-
-            <a
-              href="#contact"
-              className="p-2 hover:decoration-solid hover:underline"
-            >
-              contact me
-            </a>
-          </div>
-        </nav>
-
-        <section
-          id="about"
-          className="bg-blue-100 flex flex-col align-middle justify-center"
-        >
-          <div className="text-6xl text-center py-4">
+      <NavBar />
+      <main>
+        <section id="about" className="gsection  bg-blue-100 px-3 text-center">
+          <div className="text-4xl md:text-6xl transition ease-out duration-1000 transform hover:scale-125 cursor-pointer">
             Hi! I'm Gilberto Gabon
           </div>
-          <div className="text-4xl text-center">Fullstack Developer</div>
-        </section>
-        <section id="mywork" className="bg-blue-300  ">
-          my work
-        </section>
-        <section id="contact" className="bg-blue-400  ">
-          contact me
-        </section>
+          <div className="text-2xl md:text-4xl text-center">
+            Fullstack Developer
+          </div>
 
-        <footer className="sticky bottom-0 p-2 bg-black text-white text-center w-full">
-          Copyright
-        </footer>
-      </div>
+          <p className="text-center mt-2">
+            A passionate software developer with more than 10 years of software
+            development experience.
+          </p>
+          <p className="text-center mt-2">
+            Currently a fullstack developer using the MERN
+            (MongoDB-Express-React-NodeJS) stack. Used this stack for five (5)
+            years now.
+          </p>
+        </section>
+        <section id="mywork" className="gsection bg-blue-200">
+          <h1 className="text-4xl top-2">My Work</h1>
+          <div className="p-2">
+            <div className="flex flex-col p-4 md:flex md:flex-row md:gap-4 md:flex-wrap">
+              <div className="bg-green-100 card">
+                <h1>Work-1</h1>
+              </div>
+              <div className="bg-blue-100 card">
+                <h1>Work-2</h1>
+              </div>
+              <div className="bg-teal-100 card">
+                <h1>Work-3</h1>
+              </div>
+              <div className="bg-purple-100 card">
+                <h1>Work-4</h1>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id="contact" className="gsection bg-blue-300">
+          <h1 className="text-4xl top-2">Contact Me</h1>
+          <div className="">
+            <form action="">
+              <div className="mb-4 flex flex-col gap-1">
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" id="name" placeholder="Name" />
+              </div>
+              <div className="mb-4 flex flex-col gap-1">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Email"
+                />
+              </div>
+              <div className="mb-4 flex flex-col gap-1">
+                <label htmlFor="message">Message</label>
+                <textarea
+                  name="message"
+                  id="message"
+                  placeholder="Message"
+                  rows={4}
+                  cols={50}
+                ></textarea>
+              </div>
+              <button className="btn-primary w-full">Submit</button>
+            </form>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,108 @@
 import React from "react";
+import { useState } from "react";
+import useWindowDimensions from "../hooks/useWindowDimensions";
+
+const profile = "GAG_Profile.jpg";
 
 function NavBar() {
-  const logo = "GAG_Profile.jpg";
-  return (
-    <div className="fixed t-0 left-0">
-      <div className="mx-auto flex flex-wrap p-2 justify-between items-center bg-slate-500 ">
-        <a
-          href="#"
-          className="flex title-font font-medium items-center text-gray-900 mb-4"
-        >
-          <img src={logo} className="w-100 h-20 mr-2 rounded-full" alt="Logo" />
-        </a>
-        <div className="flex flex-wrap items-center text-base justify-center">
-          <a href="#aboutme" className="mr-5">
-            About Me
+  const { width } = useWindowDimensions();
+
+  const [burger, setBurger] = useState(true);
+  const [menu, setMenu] = useState("hidden");
+  const liClass =
+    "p-2 hover:bg-slate-300 transition ease-out duration-2000" +
+    (width >= 768 ? " hover:text-black" : "");
+
+  const doClick = (e) => {
+    e.preventDefault();
+    setMenu(
+      burger
+        ? "block text-black mt-2 bg-slate-100 border border-solid border-black rounded-b"
+        : "hidden"
+    );
+    setBurger(!burger);
+  };
+
+  const handleMenuClick = (e) => {
+    setMenu("hidden");
+    setBurger(true);
+  };
+
+  const NavSmall = () => {
+    return (
+      <nav>
+        <div className="flex justify-between flex-wrap text-white w-full fixed mx-auto top-0 h-14 p-2 bg-black z-10">
+          <a href="#about">
+            <img
+              src={profile}
+              className="w-100 h-10 mr-2 rounded-full"
+              alt="profile"
+            />
           </a>
-          <a href="#mywork" className="mr-5">
-            My Work
-          </a>
-          <a href="#contactme" className="mr-5">
-            Contact Me
-          </a>
+
+          <div>
+            <svg
+              className="md:hidden w-10 h-10 cursor-pointer"
+              data-slot="icon"
+              fill="none"
+              stroke-width="1.5"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              onClick={doClick}
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              ></path>
+            </svg>
+            <ul className={menu}>
+              <li className={liClass} onClick={handleMenuClick}>
+                <a href="#about">about me</a>
+              </li>
+              <li className={liClass} onClick={handleMenuClick}>
+                <a href="#mywork">my work</a>
+              </li>
+              <li className={liClass} onClick={handleMenuClick}>
+                <a href="#contact">contact me</a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <button className="inline-flex items-center bg-slate-700 border-0 p-3 mt-4 rounded-lg text-white">
-          Download CV
-        </button>
-      </div>
-    </div>
-  );
+      </nav>
+    );
+  };
+  const NavBig = () => {
+    return (
+      <nav>
+        <div className="flex justify-between flex-wrap text-white w-full fixed mx-auto top-0 h-14 p-2 bg-black z-10">
+          <a href="#about" className="">
+            <img
+              src={profile}
+              className="w-100 h-10 mr-2 rounded-full"
+              alt="profile"
+            />
+          </a>
+
+          <ul className="flex">
+            <li className={liClass}>
+              <a href="#about">about me</a>
+            </li>
+            <li className={liClass}>
+              <a href="#mywork">my work</a>
+            </li>
+            <li className={liClass}>
+              <a href="#contact">contact me</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  };
+
+  return <div>{width < 768 ? <NavSmall /> : <NavBig />};</div>;
 }
 
 export default NavBar;
